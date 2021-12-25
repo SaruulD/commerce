2a92c4f5ac85047d5eb8661dfb40a0ce4d527b04
Saruul <saru.dras@gmail.com>
2021-12-26 03:29:20 +0800

    Initial commit

---
+++ categories_example.js
@@ -0,0 +1,115 @@
+let categories = [
+  {
+    id: 1,
+    name: 'Category1',
+    child: {
+      name: 'Category1',
+      child: [
+        {
+          name: 'Subcategory 1',
+        },
+        {
+          name: 'Subcategory 2',
+        },
+        {
+          name: 'Subcategory 3',
+        },
+      ],
+    },
+  },
+  {
+    id: 2,
+    name: 'Category2',
+    child: {
+      name: 'Category2',
+      child: [
+        {
+          name: 'Subcategory 4',
+        },
+        {
+          name: 'Subcategory 5',
+        },
+      ],
+    },
+  },
+  {
+    id: 3,
+    name: 'Category3',
+    child: {
+      name: 'Category3',
+      child: [
+        {
+          name: 'Subcategory 6',
+        },
+        {
+          name: 'Subcategory 7',
+        },
+        {
+          name: 'Subcategory 8',
+        },
+      ],
+    },
+  },
+  {
+    id: 4,
+    name: 'Category4',
+    child: {
+      name: 'Category4',
+      child: [
+        {
+          name: 'Subcategory 9',
+        },
+        {
+          name: 'Subcategory 10',
+        },
+        {
+          name: 'Subcategory 11',
+        },
+      ],
+    },
+  },
+  {
+    id: 5,
+    name: 'Category5',
+    child: {
+      name: 'Category5',
+      child: [
+        {
+          name: 'Subcategory 12',
+        },
+        {
+          name: 'Subcategory 13',
+        },
+        {
+          name: 'Subcategory 14',
+        },
+        {
+          name: 'Subcategory 15',
+        },
+        {
+          name: 'Subcategory 16',
+        },
+      ],
+    },
+  },
+  {
+    id: 6,
+    name: 'Category6',
+    child: {
+      name: 'Category1',
+      child: [
+        {
+          name: 'Subcategory 1',
+        },
+        {
+          name: 'Subcategory 2',
+        },
+        {
+          name: 'Subcategory 3',
+        },
+      ],
+    },
+  },
+]
+
+export default categories
